487d8ee0552269a84ec0b3349ef3434af4ebbafb
Lycanroczek <kupkakupkowski856@gmail.com>
2026-08-17 12:35:44 +0200

    poprawka expansion card

--- expansion_card.py
+++ expansion_card.py
@@ -1,11 +1,9 @@
-#from machine import ADC
-#expansion = ADC(0)
-value = 65535
+from machine import ADC
+expansion = ADC(0)
 def jest_podlcaczona():
-    #value = expansion.read_u16()
-    napiecie = value * 3.3 / (65535)
-    print(napiecie)
-    if napiecie <= 1.8:
+    value = expansion.read_u16()
+    voltage = value * 3.3 / (65535)
+    if voltage <= 1.8:
         return 1
     else:
         return 0
